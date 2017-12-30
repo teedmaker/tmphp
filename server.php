333498f86112b6aea26ab71fe139b77cf27d55fd
tadeubarbosa <tadeufbarbosa@gmail.com>
2017-12-30 09:54:03 -0200

    movendo configurações de server.php para o núcleo

--- server.php
+++ server.php
@@ -1,35 +1,3 @@
 <?php
 
-$scheme   = $_SERVER['REQUEST_SCHEME'];
-$httpHost = $_SERVER['HTTP_HOST'];
-
-$scriptFileName = $_SERVER['SCRIPT_FILENAME'];
-$rootPath       = dirname($scriptFileName, 2);
-
-$base = "{$rootPath}/";
-
-$requestUri = $_SERVER['REDIRECT_URL'];
-$phpSelf    = $_SERVER['PHP_SELF'];
-$phpSelf    = dirname($phpSelf, 2);
-
-$host   = "{$scheme}://{$httpHost}{$phpSelf}/";
-$branch = str_replace("{$phpSelf}/", '', $requestUri);
-$branch = trim($branch, '/');
-
-/* host da url atual */
-define('HOST', $host);
-
-/* base do projeto */
-define('BASE', $base);
-
-/* branch da url */
-define('BRANCH', $branch);
-
-####
-
 require_once 'vendor/autoload.php';
-
-$dotenv = new Dotenv\Dotenv(__DIR__, '.env');
-$dotenv->load();
-
-// getenv('APP_ENV')
