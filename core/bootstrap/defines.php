16f63f7d7c02f9492845788054cea73af9fffb37
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-06-08 22:03:17 -0300

    correcting something in defines.php

--- core/bootstrap/defines.php
+++ core/bootstrap/defines.php
@@ -1,8 +1,10 @@
 <?php
 
+// getting a method from actual access (e.g.: get/post/put/delete)
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 define('METHOD', $method);
 
+// getting actual host
 $phpSelf  = dirname($_SERVER['PHP_SELF'], 2);
 $host     = "{$_SERVER['HTTP_HOST']}{$phpSelf}";
 $host     = str_replace('//', '/', $host);
@@ -10,15 +12,16 @@ $host     = trim($host, '/');
 $host     = "{$_SERVER['REQUEST_SCHEME']}://{$host}/";
 define('HOST', $host);
 
-$rootPath = dirname($_SERVER['SCRIPT_FILENAME'], 2);
-$base = "{$rootPath}/";
+// getting path from general files
+$base = dirname($_SERVER['SCRIPT_FILENAME'], 2) . '/';
 define('BASE', $base);
 
+// getting path from core of this project
+$core = BASE . 'core/';
+define('CORE', $core);
+
+// getting actual branch (e.g.: site.com/my/actual/branch)
 $branch = str_replace("{$phpSelf}/", '', $_SERVER['REQUEST_URI']);
 $branch = trim($branch, '/');
 $branch = empty($branch)? '/': $branch;
 define('BRANCH', $branch);
-
-$core = dirname(__FILE__, 2);
-$core = str_replace('\\', '/', $core) . '/';
-define('CORE', $core);
